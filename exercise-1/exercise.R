69d98f24b9d60d67712db1714d574617bc711225
Monica Cao <caomon@uw.edu>
2017-01-17 15:04:00 -0800

    Completed part of exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,18 +2,20 @@
 
 # Create a vector of the number of points the Seahawks scored in each game this
 # season (google "Seahawks" for the info!)
+seahawks <- c(20, 20, 25, 31, 15)
 
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each game this season
-
+more.seahawks <- c(36, 25, 31, 24, 26)
 
 # Combine your two vectors into a dataframe
-
+my.data <- data.frame(seahawks, more.seahawks)
 
 # Create a new column "diff" that is the difference in points.
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
+my.data$diff <- my.data$seahawks - my.data$more.seahawks
 
 
 # Create a new column "won" which is TRUE if the Seahawks won
